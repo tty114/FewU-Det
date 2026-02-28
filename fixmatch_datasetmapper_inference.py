cb5f20310a753bc9b66e825a2882ceb0a7bebe0d
tty114 <2925807263@qq.com>
2026-02-28 13:15:57 +0800

    fix

--- fixmatch_datasetmapper_inference.py
+++ fixmatch_datasetmapper_inference.py
@@ -85,19 +85,6 @@ def draw_bounding_boxes(
 
 
 class FixMatchDatasetMapper_Inf:
-    """
-    A callable which takes a dataset dict in Detectron2 Dataset format,
-    and map it into a format used by the model.
-
-    This is the callable to be used to map your dataset dict into fixmatch  training data.
-    See :doc:`/tutorials/data_loading` for details.
-
-    The callable currently does the following:
-
-    1. Read the image from "file_name"
-    2. Applies cropping/geometric transforms to the image and annotations
-    3. Prepare data and annotations to Tensor and :class:`Instances`
-    """
 
     @configurable
     def __init__(
